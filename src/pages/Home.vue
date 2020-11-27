5bd28fef10b002342dead738852ecba29fe8a87e
arturshmidt02 <74499402+arturshmidt02@users.noreply.github.com>
2020-11-27 13:26:24 +0600

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="wrapper-content wrapper-content--fixed">
+      <section>
+        <div class="container">
+
+          <h1>Long ago...</h1>
+          
+
+
+        </div>
+      </section>
+    </div>
+</template>
